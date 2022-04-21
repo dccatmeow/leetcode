d679c665652cc353a9e26851d3d2bd111a48ede7
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2022-04-20 20:25:55 -0400

    56. Merge Intervals

--- P56.cpp
+++ P56.cpp
@@ -9,37 +9,40 @@ Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
 Output: [[1,6],[8,10],[15,18]]
 Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 */
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        auto comp = [](vector<int> v1, vector<int> v2){
-            // sort by first element increasingly
-            // sort second element decreasingly
+        if (intervals.size() == 1){
+            return intervals;
+        }
+        // comparator sort from small to large
+        // if first element is identical, then sort second element from large to small
+        auto comp=[](vector<int>& v1, vector<int>& v2){
             if (v1[0] == v2[0]){
                 return v1[1]>v2[1];
-            }else{
-                return v1[0]<v2[0];
             }
+            return v1[0]<v2[0];
         };
         sort(intervals.begin(), intervals.end(), comp);
         vector<vector<int>> res;
-        vector<int> prev;
-        for (auto e:intervals){
-            // initial condition
-            if (prev.empty()){
-                prev = e;
+        vector<int> tmp = intervals[0];
+        for (auto v:intervals){
+            if (tmp == v){
+                // first element
                 continue;
             }
-            // check if overlapping
-            if (prev[1] >= e[0]){
-                prev[1] = max(prev[1], e[1]);
+            if (tmp[1]>=v[0]){
+                // overlap, merge
+                tmp[1] = max(tmp[1], v[1]);
             }else{
-                // not overlapping
-                res.push_back(prev);
-                prev = e;
+                //not overlap, push tmp to res
+                res.push_back(tmp);
+                tmp = v;
             }
         }
-        res.emplace_back(prev);
+        // push back last vector
+        res.push_back(tmp);
         return res;
     }
 };

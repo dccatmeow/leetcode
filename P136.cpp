13d74d2e8e6607bb450614e722922d02a97b3b08
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2022-05-18 16:28:10 -0400

    136. Single Number

--- P136.cpp
+++ P136.cpp
@@ -1,4 +1,4 @@
-/*136. Single Number
+/*
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 You must implement a solution with a linear runtime complexity and use only constant extra space.
@@ -17,6 +17,7 @@ Output: 4
 
 class Solution {
 public:
+ // XOR
     int singleNumber(vector<int>& nums) {
         int res(0);
         for (auto n:nums){

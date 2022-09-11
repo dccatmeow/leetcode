3dfd93630e117864bab64db3118d86abafd7c353
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2022-09-11 12:11:46 -0400

    419. Battleships in a Board

---
+++ P419.cpp
@@ -0,0 +1,45 @@
+/*
+Given an m x n matrix board where each cell is a battleship 'X' or empty '.', return the number of the battleships on board.
+
+Battleships can only be placed horizontally or vertically on board. In other words, 
+they can only be made of the shape 1 x k (1 row, k columns) or k x 1 (k rows, 1 column), 
+where k can be of any size. At least one horizontal or vertical cell separates between two battleships (i.e., there are no adjacent battleships).
+ */
+
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        //visited.resize(board.size());
+        //std::fill(visited.begin(), visited.end(), vector<bool>(board[0].size(), false));
+        m=board.size();
+        n=board[0].size();
+        for (int i=0; i<m; ++i){
+            for (int j=0; j<n; ++j){
+                if (board[i][j]=='.'){
+                    continue;
+                }else{
+                    dfs(board, i, j);
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+    vector<std::pair<int,int>> direction{{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(vector<vector<char>>& board, int i, int j){
+        // dfs through node i,j, mark visited node to '.'
+        board[i][j] = '.';
+        for (auto [p,q]:direction){
+            p += i;
+            q += j;
+            if (p>=0 && q>=0 && p<m && q<n && board[p][q] == 'X'){
+                dfs(board, p, q);
+            }
+        }
+    }
+private:
+    //vector<vector<bool>> visited;
+    int res=0;
+    int m;
+    int n;
+};

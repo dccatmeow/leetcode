08876570e1888dfd9d66680f37f06c3c1f64b670
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2022-09-12 15:39:46 -0400

    150. Evaluate Reverse Polish Notation

---
+++ P150.cpp
@@ -0,0 +1,68 @@
+/*
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, 
+and there will not be any division by zero operation.
+
+Example 1:
+
+Input: tokens = ["2","1","+","3","*"]
+Output: 9
+*/
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> numStk;
+        for (auto s:tokens){
+            // if s is operator
+            // pop two numbers and calculate
+            // push one number back in
+            if (s == "+"){
+                int i = numStk.top();
+                numStk.pop();
+                i += numStk.top();
+                numStk.pop();
+                numStk.push(i);
+            } else if (s == "-"){
+                int i=numStk.top();
+                numStk.pop();
+                i = numStk.top()-i;
+                numStk.pop();
+                numStk.push(i);
+            } else if (s == "*"){
+                int i=numStk.top();
+                numStk.pop();
+                i = numStk.top()*i;
+                numStk.pop();
+                numStk.push(i);
+            }else if (s== "/"){
+                int i=numStk.top();
+                numStk.pop();
+                i = numStk.top()/i;
+                numStk.pop();
+                numStk.push(i);
+            }else{
+                // negative number set flag
+                int i=0;
+                bool nflag(false);
+                for (auto c:s){
+                    if (c == '-'){
+                        nflag = true;
+                    }else{
+                        i = (c-'0')+i*10;
+                    }
+                }
+                if (nflag){
+                    i = -i;
+                }
+                numStk.push(i);
+            }
+        }
+        return numStk.top();
+    }
+};

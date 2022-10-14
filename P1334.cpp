19cf23078ea66a2b5e2faeff4304ad979c79f39e
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2022-10-14 12:00:24 -0400

    1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance

--- P1334.cpp
+++ P1334.cpp
@@ -17,33 +17,34 @@ Output: 3
 
 class Solution {
 public:
-    // Floyd Warshall Algorithm (Dynamic Programming)
+    // Floyd Warshall Algorithm 
     // Time Complexity: O(n^3)
     // Space Complexity: O(n^2)
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dp(n, vector<int>(n, DIST_MAX));
+        // g[i][j] distance from i to j
+        vector<vector<int>> g(n, vector<int>(n, DIST_MAX));
         int res(n);
-        //Initialize diagonal of dp matrix, self loop is 0
+        //Initialize diagonal of g matrix, self loop is 0
         for(int v=0; v<n; ++v){
-            dp[v][v] = 0;
+            g[v][v] = 0;
         }
         // k=0, not use any mid point from i to j
         // connected with edge has edge[2] as value
         // not connected is disconnected, default DIST_MAX
         for (auto& edge:edges){
-            dp[edge[0]][edge[1]] =edge[2];
-            dp[edge[1]][edge[0]] = edge[2];
+            g[edge[0]][edge[1]] =edge[2];
+            g[edge[1]][edge[0]] = edge[2];
             
         }
         // from i to j, using first k nodes
-        // case 1: not use node k-1, use 0....k-2 is dp[i][j][k-1]
-        // case 2: use node k-1 dp[i][k-1][k-1]+dp[k-1][j][k-1]
-        // using k nodes dp(i,j,k)= min(dp[i][j][k-1], dp[i][k-1][k-1]+dp[k-1][j][k-1] )
-        // dp[i][j] = min(dp[i][j], dp[i][k-1]+dp[k-1][j])
+        // case 1: not use node k-1, use 0....k-2 is g[i][j][k-1]
+        // case 2: use node k-1 g[i][k-1][k-1]+g[k-1][j][k-1]
+        // using k nodes g(i,j,k)= min(g[i][j][k-1], g[i][k-1][k-1]+g[k-1][j][k-1] )
+        // g[i][j] = min(g[i][j], g[i][k-1]+g[k-1][j])
         for(int k=1; k<=n;++k){
             for(int i =0; i<n;++i){
                 for (int j=0;j<n;++j){
-                    dp[i][j] = min (dp[i][j], dp[i][k-1]+dp[k-1][j]);
+                    g[i][j] = min (g[i][j], g[i][k-1]+g[k-1][j]);
                 }
             }
         }
@@ -52,14 +53,14 @@ public:
         for(int i = 0; i <n; ++i){
             int count(0);
             for(int j = 0;j <n; ++j){
-                if(dp[i][j] <= distanceThreshold)
+                if(g[i][j] <= distanceThreshold)
                     ++count;
             }
             if (count<res){
                 res = count;
                 resNode = i;
             } else if (count == res) {
-                resNode = std::max(resNode, i);
+                resNode = max(resNode, i);
             }
         }        
         

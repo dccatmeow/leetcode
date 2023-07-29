cf2f043a0d868eb784fcc5fd0f188f3f807d971b
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2023-07-29 16:31:31 -0400

    460. LFU Cache

--- P460.cpp
+++ P460.cpp
@@ -1,4 +1,6 @@
 /*
+460. LFU Cache
+
 Design and implement a data structure for a Least Frequently Used (LFU) cache.
 
 Implement the LFUCache class:
@@ -16,88 +18,56 @@ The use counter for a key in the cache is incremented either a get or put operat
 
 The functions get and put must each run in O(1) average time complexity.
 */
-
 class LFUCache {
-using pii = pair<int,int>; 
-using piter = pair< list<int>::iterator, list<pii>::iterator >;
 public:
     LFUCache(int capacity) {
-        c = capacity;
+        cap=capacity;
+        c=0;
     }
     
     int get(int key) {
-        if (c ==0){
+        if (data.find(key)==data.end()){
             return -1;
-        }
-        if (keym.count(key)){
-            int value = keym[key].second->second;
-            put(key, value);
-            return value;
         }else{
-            return -1;
+            put(key, data[key]);
+            return data[key];
         }
     }
     
     void put(int key, int value) {
-        if (c==0){
-            return;
-        }
-        if (keym.count(key)){
-            // update value
-            // get existing iterators
-            auto [fiter, iter] = keym[key];
-            // add key value
-            int newfreq = *fiter+1;
-            auto newfreqiter = fiter;
-            if (!freqm.count(newfreq)){
-                // add new frequence
-                newfreqiter = freq.insert(++newfreqiter, newfreq);
-            }else{
-                // move iterator one more
-                newfreqiter++;
+        if (data.find(key)==data.end()){
+            // add new entry
+            if (c == cap){
+                int k = freq.begin()->second.front();
+                freq.begin()->second.pop_front();
+                data.erase(k);
+                m.erase(k);
+                c--;
             }
-            freqm[newfreq].push_back(pii(key,value));
-            keym[key] = piter(newfreqiter, --(freqm[newfreq].end()) );
-            // remove old key,value pair in frequence map to key value pair
-            freqm[*fiter].erase(iter);
-            // if no element in this old frequency, remove it from map and list of frequency
-            if (freqm[*fiter].empty()){
-                freqm.erase(*fiter);
-                freq.erase(fiter);
+            c++;
+            freq[1].push_back(key);
+            auto iter = --freq[1].end();
+            m[key] = std::pair<int, list<int>::iterator> (1, iter);
+        }else{
+            // erase old entry
+            auto [f,iter] = m[key];
+            freq[f].erase(iter);
+            if (freq[f].empty()){
+                // remove empty key
+                freq.erase(f);
             }
-            return;
+            freq[f+1].push_back(key);
+            m[key] = std::pair<int, list<int>::iterator> (f+1, --freq[f+1].end());
         }
-        
-        if (count == c){
-            // remove lease frequently used
-            int oldkey = freqm[freq.front()].front().first;
-            freqm[freq.front()].pop_front();
-            // check if first key of freq is empty
-            if (freqm[freq.front()].empty()){
-                freqm.erase(freq.front());
-                freq.pop_front();
-            }
-            keym.erase(oldkey);
-            count--;
-        }
-        // add new key value
-        if (!freqm.count(1)){
-            // freqm[1] does not exist, create a new element in freq
-            freq.insert(freq.begin(),1);
-        }
-        freqm[1].push_back(pii(key, value));
-        keym[key] = piter(freq.begin(), --(freqm[1].end()) );
-        count++;
+        data[key]=value;
     }
 private:
-    // list of all the frequences e.g. 1,2,4,...
-    list<int> freq;
-    // hash table for frequence to list of key,value pair
-    unordered_map<int, list<pii>> freqm;
-    // hash table for key to frequence iterator and key,value pair iterator
-    unordered_map<int, piter> keym;
+    map<int,list<int>> freq; // frequency, list of keys
+    unordered_map<int,int> data; // key value
+    unordered_map<int, std::pair<int, list<int>::iterator> > m; // key, frequency, iterator
     int c;
-    int count = 0;
+    int cap;
+
 };
 
 /**

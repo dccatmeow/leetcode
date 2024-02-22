8714a280a79eb76f07ece81ca503619d05c99c15
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2024-02-22 11:58:51 -0500

    937. Reorder Data in Log Files

---
+++ P937.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+        // We can break this problem into two tasks: 1) Parition 2) Sort letter-logs
+        // Partition: letter-logs at the front, digit-logs at the back.
+        // We're using stable_partition instead of partition to retain the original order.
+        // stable_partition returns an iterator to the first element of the second group.
+        // first separate alphabet and number content
+        auto comp = [](const string& str){
+            // first content after identifier
+            auto idx = str.find(' ')+1; 
+            return isalpha(str[idx]);
+        };
+        auto iter = stable_partition(logs.begin(), logs.end(), comp);
+        // sort
+        auto comp2 = [](const string& str1, const string& str2){
+            // content
+            auto substr1 = string(str1.begin()+str1.find(' '), str1.end());
+            auto substr2 = string(str2.begin()+str2.find(' '), str2.end());
+            if (substr1 == substr2){
+                // compare identifier
+                return str1<str2;
+            }
+            return substr1<substr2;
+        };
+        sort(logs.begin(), iter, comp2);
+        return logs;
+
+    }
+};

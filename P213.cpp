ed1b332d2e47d9daae078d0c0eb955b3130287b0
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2025-03-26 16:18:50 -0400

    213. House Robber II

--- P213.cpp
+++ P213.cpp
@@ -9,32 +9,24 @@ Input: nums = [2,3,2]
 Output: 3
 Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
 */
-
-
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        // visited nums[0], not visited nums[n-1]
-        int tmp1, tmp2, res;
-        tmp1 = 0;
-        tmp2 = nums[0];
-        res = tmp2;
-        for (int i=1; i<n-1; ++i){
-            res = max(tmp1+nums[i], tmp2);
-            tmp1 = tmp2;
-            tmp2 = res;
+        int n=nums.size();
+        if (n==1) return nums[0];
+        int res1 = findMaxRob(0,nums[0],n-1, nums);
+        int res2 = findMaxRob(0,0,n, nums);
+        return max(res1,res2);
+    }
+
+    int findMaxRob(int tmp, int tmp1, int m, vector<int>& nums){
+        for (int i=1;i<m;++i){
+            // 0 is robbed, n-1 is not robbed 
+            int res = max(tmp+nums[i], tmp1);
+            // move 1 forward
+            tmp = tmp1;
+            tmp1 = res;
         }
-        int res1(0);
-        tmp1 = 0;
-        tmp2 = 0;
-        // not visit nums[0]
-        for (int i=1; i<n; ++i){
-            res1 = max(tmp1+nums[i], tmp2);
-            tmp1 = tmp2;
-            tmp2 = res1;
-        }
-        return max(res, res1);
-        
+        return max(tmp,tmp1);
     }
 };

fb19bd84b174e65f59f7310f4ff621c87175d4dd
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2026-01-15 20:50:22 -0500

    3. Longest Substring Without Repeating Characters

--- P3.cpp
+++ P3.cpp
@@ -15,28 +15,27 @@ Explanation: The answer is "b", with the length of 1.
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        if (n==0) return 0;
-        if (n==1) return 1;
-        int res=0;
-        unordered_map<char,int> m;
-        int l(0);
-        for (int i=0;i<n;++i){
-            if (m.find(s[i])!=m.end() && m[s[i]]!=-1){
-                res = max (res, i - l);
-                // invalid from l to tmp-1
-                // l....m[s[i]]....i
-                int tmp = m[s[i]];
-                for (int j=l;j<tmp;++j){
-                    m[s[j]] = -1;
+        unordered_map<int,int> m;
+        int start(0),res(0);
+        for (int i=0;i<s.size();++i){
+            char tmp = s[i];
+            if (m.find(tmp)!=m.end()){
+                // find duplicate and erase from m
+                if (start==0){
+                    res = max(res, i);
+                }else{
+                    res = max(res, i-start);
                 }
-                l = tmp+1;
+                for (int j=start; j<m[tmp];j++){
+                    m.erase(s[j]);
+                }
+                start = m[tmp]+1;
             }
-            m[s[i]] = i;
+            m[tmp]=i;
         }
-        if (l!=n-1){
-            res = max(res, n-l);
-        }
-        return res;
+        // count last case in m
+        int last = s.size()-start;
+        res = max(res, last);
+        return res;   
     }
 };

5741972dfb027f67acd4d618363e0cc6b4dc0894
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2026-01-17 16:52:25 -0500

    1293. Shortest Path in a Grid with Obstacles Elimination

--- P1293.cpp
+++ P1293.cpp
@@ -5,47 +5,58 @@ You can move up, down, left, or right from and to an empty cell in one step.
 Return the minimum number of steps to walk from the upper left corner (0, 0) to the lower right corner (m - 1, n - 1) 
 given that you can eliminate at most k obstacles. If it is not possible to find such walk return -1.
 */
-
 class Solution {
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        vector<pair<int, int>> direction{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int m=grid.size();
-        int n=grid[0].size();
-        bool visited[m][n][k+1];
-        memset(visited, 0, sizeof(visited));
-        
-        // BFS
-        int res(0);
-        // queue with element {i, j, #of obstacle}
+        if (grid.empty()) return 0;
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> visited(m,vector<int>(n,INT_MAX));
+        auto direction = vector<std::pair<int,int>>{{1,0},{0,1},{-1,0},{0,-1}};
         queue<vector<int>> q;
         q.push(vector<int>{0,0,0});
+        visited[0][0] = 0;
+        int step(0);
         while(!q.empty()){
-            int sz=q.size();
-            for(int i=0; i<sz; ++i){
+            int sz = q.size();
+            for (int qsz = 0; qsz<sz;++qsz){
                 auto v = q.front();
-                if (v[0] == m-1 && v[1] == n-1){
-                    // reach [m-1, n-1]
-                    return res;
-                }
-                //visited[v[0]][v[1]][v[2]] = true; only put visited flag at pop() in dijkistra
                 q.pop();
-                for (auto [x,y]:direction){
-                    x += v[0];
-                    y += v[1];
-                    if (x<0 || y<0 || x>m-1 || y>n-1){
+                int i=v[0];
+                int j=v[1];
+                int obstacle = v[2];
+                
+                if (i==m-1&&j==n-1){
+                    // find target
+                    return step;
+                }
+                // find shortest path in each direction
+                for (auto [a,b]:direction){
+                    a += i;
+                    b += j;
+                    int o = obstacle;
+                    if (a<0||b<0||a>=m||b>=n){
+                    // out of boundary
                         continue;
                     }
-                    int z = v[2]+grid[x][y];
-                    if (z>k || visited[x][y][z]){
+                    if (grid[a][b] ==1) {
+                        o++;
+                    }
+                    if (o>k){
                         continue;
                     }
-                    q.push({x, y, z});
-                    // BFS mark visited when it is pushed into queue
-                    visited[x][y][z] = true;
+                    if (o>=visited[a][b]){
+                        // visited
+                        continue;
+                    }
+                    // set flag
+                    visited[a][b] = o;
+                    q.push(vector<int>{a,b,o});
+                    
+                    
                 }
             }
-            res++;
+            step++;
         }
         return -1;
     }

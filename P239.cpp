9efed2c4a7f8879ee7e733690d56adb98459d313
dccatmeow <100095842+dccatmeow@users.noreply.github.com>
2026-01-20 21:33:50 -0500

    Sliding window maximum

--- P239.cpp
+++ P239.cpp
@@ -11,33 +11,21 @@ Output: [3,3,5,5,6,7]
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // [1, 3, -1, -3, -4], k=3
-        // store index of nums in dq
-        // pop any value nums[dq.back()] that is smaller than nums[i] from back of dq
-        // as these number is no longer needed
-        // [0]
-        // [1]
-        // [1,2] // push back nums[dq.front()] to res, because 3 element is there
-        // [1,2,3] // push back nums[dq.front()] to res
-        // i=4, dq=[1,2,3]->[2,3,4]
-        // pop front as new element -4's sliding window does not include 3, i-dq.front()=4-1=3
-        // push back nums[dq.front()] to res
-        // res = 
+        // use deque as window
         deque<int> dq;
         vector<int> res;
-        int l = nums.size();
-        for (int i=0;i<l;++i){
-            if (!dq.empty() && (i-dq.front())==k){
-                // the window size is k 
-                // when i-dq.front() == k, it means the sliding window left side should move
+        for (int i=0; i<nums.size();++i){
+            // pop when windows size is larger than k
+            if (!dq.empty() && i-dq.front()>=k){
                 dq.pop_front();
             }
+            // mono decrease queue, storing index
             while(!dq.empty() && nums[dq.back()]<nums[i]){
-                // dq.back is less than the value nums[i] will be pop
+                // pop back, until back is greater than nums[i]
                 dq.pop_back();
             }
             dq.push_back(i);
-            // save in res
+            // store max value which is the queue front
             if (i>=k-1){
                 res.push_back(nums[dq.front()]);
             }
